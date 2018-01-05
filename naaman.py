0e80fc85d16299aacb295cc54502dabc2a025bf3
Sean Enck <enckse@gmail.com>
2018-01-05 16:47:23 -0500

    arg checking in place.

--- naaman.py
+++ naaman.py
@@ -14,14 +14,59 @@ logger = logging.getLogger(_NAME)
 console_format = logging.Formatter('%(message)s')
 file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
+def _console_output(string, prefix="", callback=logger.info):
+    """console/pretty output."""
+    callback("{} => {}".format(prefix, string))
+
+
+def _console_error(string):
+    _console_output(string, prefix="[ERROR]", callback=logger.error)
 
 def _validate_options(args):
     valid_count = 0
+    invalid = False
+    def call_on(name):
+        _console_output("performing {}".format(name))
     if args.sync:
+        call_on("sync")
+        valid_count += 1
+
+    if args.upgrade:
+        call_on("upgrade")
+        valid_count += 1
+
+    if args.remove:
+        call_on("remove")
+        valid_count += 1
+
+    if args.query:
+        call_on("query")
         valid_count += 1
 
     if valid_count != 1:
-        logger.warn("i don't know how to proceed!")
+        _console_error("multiple top-level arguments request (this is invalid)")
+        invalid = True
+   
+    if args.list:
+        if args.query:
+            _console_output("get listing")
+        else:
+            _console_error("list only available via query")
+            invalid = True
+
+    if args.upgrades:
+        if args.upgrade or args.sync:
+            _console_output("upgrading packages")
+        else:
+            _console_error("upgrade not available via these functions")
+            invalid = True
+
+    if args.search:
+        if args.sync:
+            _console_output("searching {}".format(args.search))
+        else:
+            _console_error("search only available via sync")
+    if invalid:
         exit(1)
 
 def main():
