34b58f360796e9e52439594473cd17dd58b63c61
Sean Enck <enckse@gmail.com>
2018-01-05 16:03:49 -0500

    naaman begins

---
+++ naaman.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+N(ot) A(nother) A(UR) Man(ager)
+
+Is an AUR wrapper/manager that uses pacman as it's backing data store.
+"""
+
+def main():
+    """Entry point."""
+
+
+if __name__ == "__main__":
+    main()
