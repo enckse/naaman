1b72d964f3f04cfb0d46f819144fdf2b29dbf646
Sean Enck <enckse@gmail.com>
2018-01-05 21:54:08 -0500

    search in place

--- naaman.py
+++ naaman.py
@@ -11,11 +11,27 @@ from xdg import BaseDirectory
 import getpass
 import pyalpm
 from pycman import config
+import urllib.request
+import urllib.parse
+import json
+import string
 
 _NAME = "naaman"
 logger = logging.getLogger(_NAME)
 console_format = logging.Formatter('%(message)s')
 file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+
+_AUR = "https://aur.archlinux.org{}"
+_RESULT_JSON = 'results'
+_AUR_NAME = "Name"
+_AUR_DESC = "Description"
+_AUR_URL = _AUR.format("/rpc?v=5&type=search&by={}&arg={}")
+_AUR_VERS = "Version"
+_AUR_URLP = "URLPath"
+_AUR_NAME_DESC_TYPE = "name-desc"
+_AUR_NAME_TYPE = "name"
+_PRINTABLE = set(string.printable)
 
 
 def _console_output(string, prefix="", callback=logger.info):
@@ -44,9 +60,16 @@ def _validate_options(args, unknown):
     valid_count = 0
     invalid = False
     need_targets = False
+    call_on = None
 
-    def call_on(name):
-        _console_output("performing {}".format(name))
+    if args.search or args.query:
+        def no_call(name):
+            pass
+        call_on = no_call
+    else:
+        def action(name):
+            _console_output("performing {}".format(name))
+        call_on = action
 
     if args.sync:
         call_on("sync")
@@ -92,19 +115,81 @@ def _validate_options(args, unknown):
         callback = _query
     if args.search:
         callback = _search
-    if args.sync:
+    if args.sync and not args.search:
         # this handles upgrades and sync (for now)
         callback = _sync_upgrade
     if args.remove:
         callback = _remove
 
-    if callback is None:
+    if not invalid and callback is None:
         _console_error("unable to find callback")
         invalid = True
 
     if invalid:
         exit(1)
     callback(ctx)
+
+
+def _sync_upgrade(context):
+    """Sync upgrade."""
+
+def _remove(context):
+    """Remove package."""
+
+def _get_segment(j, key):
+    """Get an ascii printable segment."""
+    inputs = j[key]
+    if inputs is None:
+        return ""
+    res = "".join([x for x in inputs if x in _PRINTABLE])
+    if len(inputs) != len(res):
+        logger.debug("dropped non-ascii characters")
+    return res
+
+
+def _rpc_search(package_name, typed, exact, context):
+    """Search for a package in the aur."""
+    url = _AUR_URL
+    url = url.format(typed, urllib.parse.quote(package_name))
+    try:
+        with urllib.request.urlopen(url) as req:
+            result = req.read()
+            j = json.loads(result.decode("utf-8"))
+            if _RESULT_JSON in j:
+                for result in j[_RESULT_JSON]:
+                    try:
+                        name = _get_segment(result, _AUR_NAME)
+                        desc = _get_segment(result, _AUR_DESC)
+                        vers = _get_segment(result, _AUR_VERS)
+                        if exact:
+                            if name == package_name:
+                                return (name, vers, result[_AUR_URLP])
+                        else:
+                            ind = ""
+                            if not name or not desc or not vers:
+                                logger.debug("unable to read this package")
+                                logger.debug(result)
+                                continue
+                            if context.db.get_pkg(name) is not None:
+                                ind = " [installed]"
+                            logger.info("aur/{} {}{}".format(name, vers, ind))
+                            logger.info("    {}".format(desc))
+                    except Exception as e:
+                        logger.error("unable to parse package")
+                        logger.error(e)
+                        logger.debug(result)
+                        break
+    except Exception as e:
+        logger.error("error calling AUR search")
+        logger.error(e)
+
+
+
+def _search(context):
+    """Perform a search."""
+    for target in context.targets:
+        logger.debug("searching for {}".format(target))
+        _rpc_search(target, _AUR_NAME_DESC_TYPE, False, context)
 
 def _query(context):
     """Query pacman."""
