11f4e59935e72159f158a08d86436ff3db30f6cd
Sean Enck <enckse@gmail.com>
2018-01-10 11:49:54 -0500

    update version

--- naaman.py
+++ naaman.py
@@ -21,7 +21,7 @@ from datetime import datetime, timedelta
 from xdg import BaseDirectory
 from pycman import config
 
-_VERSION = "0.2.2"
+_VERSION = "0.2.3"
 _NAME = "naaman"
 _CONFIG = _NAME + ".conf"
 logger = logging.getLogger(_NAME)
