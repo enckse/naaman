f2ff2055b74e072f5d92221343c4c0d9155f46c2
Sean Enck <enckse@gmail.com>
2018-01-27 19:25:38 -0500

    version bump

--- naaman.py
+++ naaman.py
@@ -22,7 +22,7 @@ from datetime import datetime, timedelta
 from xdg import BaseDirectory
 from pycman import config
 
-_VERSION = "0.4.2"
+_VERSION = "0.5.0"
 _NAME = "naaman"
 _CONFIG = _NAME + ".conf"
 logger = logging.getLogger(_NAME)
