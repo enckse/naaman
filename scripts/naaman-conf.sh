0e7a51ae73677249b3531741df1e07c4fb5073c8
Sean Enck <enckse@gmail.com>
2018-03-21 12:49:12 -0400

    cleaning up docs, will not generate anymore

--- scripts/naaman-conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for options in $(cat naaman.conf | grep -v "^#" | cut -d "=" -f 1); do
-    cat scripts/naaman.conf.5 | grep -q "^$options$"
-    if [ $? -ne 0 ]; then
-        echo "missing $options man entry for naaman.conf"
-        exit 1
-    fi
-done
--- scripts/naaman.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-MANPY="bin/man.py"
-cat naaman.py | grep -v "import pyalpm" | grep -v "^from pycman" > $MANPY
-python $MANPY $@
