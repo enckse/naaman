f9985182b3e2f654f19a5c4cff2e719997169baf
Sean Enck <enckse@gmail.com>
2018-03-22 09:18:41 -0400

    --fetch = -f and new --fetch-dir coming

--- naaman/arguments/syncup.py
+++ naaman/arguments/syncup.py
@@ -91,10 +91,14 @@ regardless of what is currently installed. disabling this would save bandwidth
 any speciality checking (e.g. --vcs-install-only). Use this flag to update all
 AUR packages on the system""",
                        action='store_true')
-    group.add_argument("--fetch",
+    group.add_argument('-f', "--fetch",
                        action='store_true',
                        help="""Normally naaman will manage source retrieval and
 package building without problem. Though this relies on knowing a PKGBUILD is
 safe to use and install. Utilize this option to tell naaman to fetch to a
 location and not manage the files/perform the build. The user can then verify
 the PKGBUILD and run makepkg manually.""")
+    group.add_argument('--fetch-dir',
+                       help="""Specifies the location to store fetched
+(--fetch) packages on the file system. Whenever fetch is used this is the
+directory that naaman will write to.""")
